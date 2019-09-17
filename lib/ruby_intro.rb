89bd14daef6b6f55a64e77777556c10ee661d7d1
Patrick Palmer <patrickpalmer271@gmail.com>
2019-09-17 18:44:21 -0500

    Finished the rest of the problem set and all tests passed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -45,7 +45,6 @@ def hello(name)
 end 
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
   vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
   if s.empty?
     return false
@@ -61,22 +60,60 @@ def starts_with_consonant? s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  binary = is_binary(s)
+  if !binary 
+    return false
+  else 
+    num = s.to_i(2)
+    if num%4 == 0
+      return true
+    end
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price #https://dev.to/k_penguin_sato/ruby-getters-and-setters-1p30
+  
+  def initialize(isbn, price)
+    raise ArgumentError.new("ISBN must have data") if isbn.empty?
+    raise ArgumentError.new("Price must be positive") if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn #getter method
+    @isbn
+  end
+
+  def price 
+    @price
+  end
+
+  def price_as_string
+    string = "$" + ('%.2f' % @price).to_s
+    return string
+  end
 end
 
 
+#---------------------helper functions---------------------
 class String #https://stackoverflow.com/questions/2730854/ruby-how-to-get-the-first-character-of-a-string
   def initial
     self[0,1]
   end
 end
 
-def letter?(lookAhead) #https://stackoverflow.com/questions/14551256/ruby-how-to-find-out-if-a-character-is-a-letter-or-a-digit
-  lookAhead =~ /[A-Za-z]/
+def letter?(s) #https://stackoverflow.com/questions/14551256/ruby-how-to-find-out-if-a-character-is-a-letter-or-a-digit
+  s =~ /[A-Za-z]/
+end
+
+def is_binary(s)
+  if s.match("^[01]+$")
+    return true
+  else
+    return false
+  end
 end
