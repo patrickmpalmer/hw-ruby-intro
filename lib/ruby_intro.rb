f4b6d80332c51b07bc3e22d952f5cbc26f620848
Patrick Palmer <patrickpalmer271@gmail.com>
2019-09-17 17:11:16 -0500

    Finished Section 0 Problem 1 and 2 and Section 2 Problem 0 and 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,21 +7,57 @@ def sum arr
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr = arr.sort { |a, b| b <=> a } #https://mixandgo.com/learn/ruby-sort-and-sort-by
+  if arr.length >= 2
+    return arr[0] + arr[1]
+  elsif arr.length == 1
+    return arr[0]
+  else
+    return 0
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # greedy algo time
+  if arr.length == 0 || arr.length == 1
+    return false
+  end
+  arr.each do |num|
+    arr.each do |num2|
+      if num == num2
+        break
+      end
+      testnum = num + num2
+      if testnum == n
+        #print "num: ", num, " num2: ", num2, " testnum: ", testnum, "\n"
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
-end
+  string = "Hello, " + name
+  return string
+end 
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+  if s.empty?
+    return false
+  elsif
+    !letter? s.initial
+    return false
+  end
+  if vowels.include? s.initial
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
@@ -32,4 +68,15 @@ end
 
 class BookInStock
 # YOUR CODE HERE
+end
+
+
+class String #https://stackoverflow.com/questions/2730854/ruby-how-to-get-the-first-character-of-a-string
+  def initial
+    self[0,1]
+  end
+end
+
+def letter?(lookAhead) #https://stackoverflow.com/questions/14551256/ruby-how-to-find-out-if-a-character-is-a-letter-or-a-digit
+  lookAhead =~ /[A-Za-z]/
 end
